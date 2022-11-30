d2696c2cdad2ed0626618425a280c02328fefe3e
Javi Corvi <javicorvi@gmail.com>
2022-11-30 11:17:07 +0100

    new version with parametrization

--- process_terms.py
+++ process_terms.py
@@ -1,24 +1,33 @@
 ##import all required packages 
 import pymongo
+import argparse
 from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
+import sys
 from datetime import datetime
-
-load_dotenv()
 
 ##connect to debbie mongodb
 #database_url = os.environ.get('DATABASE_URL')
 
-database_url = os.environ.get('DATABASE_URL')
+parser = argparse.ArgumentParser()
+parser.add_argument('-db', help= 'Database url. The mongo connection.')
+parser.add_argument('-j', help= 'Collection prefix.')
+args = parser.parse_args()
+if (args.db == None):
+	print("Please set database url parameter")
+	sys.exit(1)
+if (args.j == None):
+	print("Please set the collection prefix. Example test, dev, or production (production environment lookout).")
+	sys.exit(1)
+
+database_url = args.db
 client = MongoClient(database_url, maxPoolSize=None)
 db = client ['DEBBIE']
-
+collection_prefix = args.j
 # assign the collection as 'annotations'
-terms = db.production_terms
-annotations = db.production_annotations
-production_terms_by_years = db.production_terms_by_years
-production_execution = db.production_execution
+terms = db[collection_prefix+'_terms']
+annotations = db[collection_prefix+'_annotations']
+terms_by_years = db[collection_prefix+'_terms_by_years']
+execution = db[collection_prefix+'_execution']
 def process():
 	print ("process starts")
 	
@@ -27,17 +36,17 @@ def process():
 	
 	
 	print("terms inserts process starts.") 
-	result_terms = annotations.aggregate([{'$project':
+	annotations.aggregate([{'$project':
 	{"text":"$annotations.lemma"}
 	},
 	{"$unwind": "$text" },
 	{"$group": { "_id": "$text", "total": { "$sum": 1 }}},
-    {"$sort":{"total":-1}}, { "$merge": { "into": "production_terms" } }
+    {"$sort":{"total":-1}}, { "$merge": { "into": collection_prefix+"_terms" } }
 	])
-	count = db.production_terms.count()
+	count = db[collection_prefix+'_terms'].count()
 	print("terms inserts process ends. total inserted: " + str(count))
 	
-	x = production_terms_by_years.delete_many({})
+	x = terms_by_years.delete_many({})
 	print(x.deleted_count, " terms deleted.") 
 	
 	print("year precalculations by term inserts starts")
@@ -53,20 +62,17 @@ def process():
 				{'$project':{'text':'$_id.text', 'year':'$_id.year', 'count':1 , "info_2":{ "pmids":"$info"}, '_id':0}},
 				{"$group": { "_id": { "text":"$text"}, "years": { "$addToSet": {"year":"$year","count":"$count"}}}},
 				
-				{ "$merge": { "into": "production_terms_by_years" } }
+				{ "$merge": { "into": collection_prefix+"_terms_by_years" } }
 				], allowDiskUse=True)
-	count2 = db.production_terms.count()
+	count2 = db[collection_prefix+'_terms_by_years'].count()
 	print("year precalculations by term inserts ends. total inserted: " + str(count2))
-	
 	
 	# Creating Dictionary of records to be
 	# inserted
 	d = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 	record = { "date": d, "comment": "DEBBIE terms preprocessed","terms_processed": count}
-	production_execution.insert_one(record)
+	execution.insert_one(record)
 	
 	print ("process end")
-	
-	
 	
 process()
